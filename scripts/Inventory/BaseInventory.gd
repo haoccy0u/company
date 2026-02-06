28bedf3327c8144077ed547c8bff66e01de7d4b7
haoccyou <haoccyou@gmail.com>
2026-02-06 22:55:56 +0800

    inventory update

--- scripts/Inventory/BaseInventory.gd
+++ scripts/Inventory/BaseInventory.gd
@@ -1,5 +1,5 @@
 extends Resource
-class_name Inventory
+class_name BaseInventory
 
 @export var name: String
 @export var size: int
--- scripts/Inventory/item_scene.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-class_name ItemScene
-
-var item_data: BaseItemData
-var texture: Texture2D 
-
-func _init(item_data: BaseItemData) -> void:
-	texture = item_data.texture
